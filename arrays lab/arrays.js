10a84b761c4db0452c1d9210d0f3678685d4d026
kigwa ian <kigwaian99@gmail.com>
2024-07-26 11:59:49 +0300

    first commit

---
+++ arrays lab/arrays.js
@@ -0,0 +1,15 @@
+const record1 = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+  ];
+  
+  const record2 = [
+    { year: "2015", result: "N/A" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+  ];
+  
+  console.log(superbowlWin(record1)); 
+  console.log(superbowlWin(record2)); 
+  
